7e8ef3e04a25732e83a3544aa0205272533788c4
Valentin Alberto <valalberto0214@gmail.com>
2023-12-12 22:55:20 +0800

    SPT-12: create user model
    add pojo for user

---
+++ src/main/java/com/via/cms/model/User.java
@@ -0,0 +1,57 @@
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2023. All Rights Reserved.
+ * 
+ */
+package com.via.cms.model;
+
+import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**
+ * 
+ * @author via
+ * 
+ * @date 12 Dec 2023
+ *
+ */
+@Data
+@Entity
+// @DynamicUpdate
+// @DynamicInsert
+@Table(name = "user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "contactNumber")
+    private String contactNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "role")
+    private String role;
+
+}

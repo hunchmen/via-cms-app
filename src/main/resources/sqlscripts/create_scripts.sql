b52a04b0b7f6d40cea1a77fc3a78097c8a1957d8
Valentin Alberto <valalberto0214@gmail.com>
2023-12-13 19:03:54 +0800

    SPT-22: update create_scripts.sql (#12)
    * SPT-12: create user model
    
    add pojo for user
    
    * SPT-12: create user model
    
    update pojo for user
    
    * SPT-13: Create controller for user
    
    add UserController and UserControllerImpl.java
    
    * SPT-14: create service layer for user signup
    
    * SPT-15: create repository layer for user signup
    
    * SPT-16; creaet constant class
    
    * SPT-17: craete util class
    
    * SPT-19: add devtools in pom.xml
    
    * SPT-20: update User model
    
    * SPT-21: upate application.properties
    
    * SPT-22: update create_scripts.sql

--- src/main/resources/sqlscripts/create_scripts.sql
+++ src/main/resources/sqlscripts/create_scripts.sql
@@ -1,1 +1,13 @@
 CREATE SCHEMA `cafe_management_db` ;
+
+
+create table user (
+        id integer not null auto_increment,
+        contact_number varchar(255),
+        email varchar(255),
+        name varchar(255),
+        password varchar(255),
+        role varchar(255),
+        status varchar(255),
+        primary key (id)
+    ) engine=InnoDB

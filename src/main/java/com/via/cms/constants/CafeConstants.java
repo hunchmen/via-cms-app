135a028a5328e8a2f621fa23c2f9b87060c49ad2
Valentin Alberto <valalberto0214@gmail.com>
2023-12-13 17:42:40 +0800

    SPT-16; creaet constant class

---
+++ src/main/java/com/via/cms/constants/CafeConstants.java
@@ -0,0 +1,24 @@
+/**
+ * Licensed Materials - Property of hunchmen
+ * 
+ * (C) hunchmen. 2023. All Rights Reserved.
+ * 
+ */
+package com.via.cms.constants;
+
+/**
+ * 
+ * @author via
+ * 
+ * @date 12 Dec 2023
+ *
+ */
+public class CafeConstants {
+
+    private CafeConstants() {}
+
+    public static final String SOMETHING_WENT_WRONG = "Something went wrong";
+
+    public static final String INVALID_DATA = "Invalid data";
+
+}
